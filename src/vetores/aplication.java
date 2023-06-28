9a3800a43ef2f9c9782e04f48800dda0e5bf3a5f
Rodrigo Oliveira <rodrigo.olv.santos@gmail.com>
2023-06-28 18:42:50 -0300

    Trabalhando com vetores

---
+++ src/vetores/aplication.java
@@ -0,0 +1,35 @@
+package vetores;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class aplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Digite um limitador de alturas");
+		
+		int n = sc.nextInt();
+		double[] vect = new double [n]; //Criando um vector com n elementos.
+		
+		for (int i=0; i<n; i++){
+			vect[i] = sc.nextDouble();
+			
+		}
+		double sum = 0.0;
+		for (int i=0; i<n; i++) {
+			sum += vect[i];
+		}
+		
+		double avg = sum / n;
+				
+	   System.out.printf("AVERAGE HEIGHT: %.2f%n",avg);
+				 				
+	    sc.close();
+	}
+
+}
